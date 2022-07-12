00dc7ccb6630b963d0e3029001d2fc87f3749e96
UchechukwuOnuoha <UchechukwuOnuoha@DESKTOP-5Q2J00P>
2022-07-12 12:01:13 +0100

    Added rest API for both minimum and Maximum steps;

--- src/main/java/co/mvpfactory/maze/mazeprocessor/MazeGridProcessor.java
+++ src/main/java/co/mvpfactory/maze/mazeprocessor/MazeGridProcessor.java
@@ -46,7 +46,7 @@ public class MazeGridProcessor {
                 custMaze.reset();
             }
             int minimumSize = custMaze.getGridLenght();
-                 for (ArrayList<String> path : paths) {
+            for (ArrayList<String> path : paths) {
                 if (path.size() < minimumSize && !path.isEmpty()) {
                     minimumSize = path.size();
                     minimumStep = path;
@@ -59,6 +59,46 @@ public class MazeGridProcessor {
         }
         custMaze = null;
         return minimumStep;
+    }
+
+    public ArrayList<String> getMazeMaximumStep() {
+        ArrayList<String> maximumStep = new ArrayList<>();
+        CustomMaze custMaze = null;
+        try {
+            custMaze = new CustomMaze(walls, entrance, gridSize);
+            custMaze.initializeCustomMaze();
+            List<Coordinate> possibleExits = custMaze.getGridExitCordinate().possibleExits;
+            if (possibleExits.isEmpty()) {
+                throw new BadRequestException("Maze Grid has no possible exit path/point");
+            }
+            LongestMazePathSolver longestMazeSolver = new LongestMazePathSolver();
+            ArrayList<ArrayList<String>> paths = new ArrayList<>();
+            for (Coordinate cord : possibleExits) {
+
+                List<Coordinate> path = longestMazeSolver.findLongestPath(custMaze.getCustomIntegerMaze(), custMaze.start, cord);
+                ArrayList<String> step = new ArrayList<>();
+                if (path != null) {
+                    for (Coordinate coordinate : path) {
+                        step.add(custMaze.alphaNumericMaze[coordinate.getX()][coordinate.getY()]);
+                    }
+                    paths.add(step);
+                }
+               
+            }
+            int maximumSize = 0;
+            for (ArrayList<String> path : paths) {
+                if (path.size() > maximumSize && !path.isEmpty()) {
+                    maximumSize = path.size();
+                    maximumStep = path;
+                }
+
+            }
+
+        } catch (GenericMazeServiceException ex) {
+            throw new GenericMazeServiceException(ex.getMessage());
+        }
+        custMaze = null;
+        return maximumStep;
     }
 
 }
